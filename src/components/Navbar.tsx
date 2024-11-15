4baab63e63a98af5223cbacc1e6ed801eba44cee
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-15 22:40:53 +0000

    Navbar component added to the project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="fixed top-2 z-50 w-screen px-4">
+        <div className="container flex items-center justify-between rounded-lg 
+        bg-black py-3">
+            <div className="flex flex-shrink-0 items-center justify-between">
+                
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
