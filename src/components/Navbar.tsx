2fae2bbc62dcb06b15f13fa31768f0c20d341cad
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-15 23:05:25 +0000

    Navbar component styled with tailwind css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,7 @@
+import { NAV_LINKS } from "@/constants"
 import Image from "next/image"
+import Link from "next/link"
+import { Button } from "./ui/button"
 
 const Navbar = () => {
   return (
@@ -14,6 +17,27 @@ const Navbar = () => {
                 alt="Logo" 
                 />
                 <span className="text-sm tracking-tight text-white">RealES</span>
+            </div>
+            <div className="hidden lg:flex">
+                <ul className="flex items-center gap-4">
+                    {NAV_LINKS.map((item, index) => (
+                        <li key={index}>
+                            <Link className="text-sm text-white hover:text-neutral-500" 
+                            href={item.url}
+                            >
+                            {item.title}    
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="hidden text-sm text-white lg:flex">
+                <Button variant="outline" className="mr-2 bg-black">
+                    Sign In
+                </Button>
+                <Button className="bg-slate-800">
+                    Sign Up
+                </Button>
             </div>
         </div>
     </nav>
