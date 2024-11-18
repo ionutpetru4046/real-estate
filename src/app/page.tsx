54e44cd6dce44a9c46765ccf71fa7ca353e4ab45
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-18 19:46:15 +0000

    SearchForm added to the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import SearchForm from "@/components/SearchForm";
 
 const Home = () => {
   return (
     <>
      <Navbar />
      <Hero />
+     <SearchForm />
     </>
   )
 }
