9a3979b700a800c27033674b7d405ea38fbe6dd7
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-22 13:53:06 +0000

    styling added to the HowItWorks compoenent

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -1,9 +1,26 @@
+import { HOW_IT_WORKS } from "@/constants"
+import { CircleCheckBig } from "lucide-react";
+
 const HowItWorks = () => {
   return (
     <div className="container mx-auto my-10 border-b pd-10">
-      
+      <h2 className="text-center text-3xl tracking-tighter sm:text-4xl 
+      lg:text-5xl">
+        {HOW_IT_WORKS.title}
+      </h2>
+      <p className="mx-auto my-10 max-w-4xl text-center text-neutral-800">{HOW_IT_WORKS.content}</p>
+      <div className="works relative h-[640px] rounded-xl">
+        <div className="absolute left-0 top-0 h-full overflow-hidden 
+        p-10 md:p-20">
+            {HOW_IT_WORKS.steps.map((step, index) => (
+                <div key={index}>
+                    <CircleCheckBig />
+                </div>
+            ))}
+        </div>
+      </div>
     </div>
   )
 }
 
-export default HowItWorks
+export default HowItWorks;
