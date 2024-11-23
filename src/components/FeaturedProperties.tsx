909a5df4ff418f056d47a859cd6f118b9c5ec6df
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-23 17:32:27 +0000

    styling added to featured component

--- src/components/FeaturedProperties.tsx
+++ src/components/FeaturedProperties.tsx
@@ -1,7 +1,9 @@
 function FeaturedProperties() {
   return (
-    <div>
-        
+    <div className="container mx-auto border-b py-6 pb-10">
+        <h2 className="text-center text-3xl tracking-tighter sm:text-4xl lg:text-5xl">
+            Featured
+        </h2>
     </div>
   )
 }
