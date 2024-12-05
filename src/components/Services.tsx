7c4f6f71e50530a0947a73768c4311a87cc8d2d0
Coste Ionut Petru <ionut4046@yahoo.com>
2024-12-05 16:50:10 +0000

    styling and title added to the component

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,7 +1,9 @@
 const Services = () => {
   return (
-    <div>
-      
+    <div className="container mx-auto my-8 border-b pb-10">
+      <h2 className="mb-20 text-center text-3xl tracking-tighter sm:text-4xl lg:5xl">
+        Services
+      </h2>
     </div>
   )
 }
