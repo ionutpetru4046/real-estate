3776f37c1f5c6e4f184261d95d55e120ae4c6d9e
Coste Ionut Petru <ionut4046@yahoo.com>
2024-12-06 17:44:55 +0000

    h2 title added to the About Us component

--- src/components/ui/AboutUs.tsx
+++ src/components/ui/AboutUs.tsx
@@ -1,7 +1,12 @@
 const AboutUs = () => {
   return (
-    <div>
-      
+    <div className="container mx-auto my-20 border-b pb-10">
+      <div className="mb-8">
+        <h2 className="mb-6 text-center text-3xl tracking-tighter sm:text-4xl 
+        lg:text-5xl">
+          About Us
+        </h2>
+      </div>
     </div>
   )
 }
